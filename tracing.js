71dc4fdd30ad1e4b4320b03268d5d19733f9a6bd
sethMKennedy <62225478+sethMKennedy@users.noreply.github.com>
2024-04-23 23:42:42 -0400

    initial commit

---
+++ tracing.js
@@ -0,0 +1,57 @@
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
+const {
+  getNodeAutoInstrumentations,
+} = require('@opentelemetry/auto-instrumentations-node');
+const {
+  PeriodicExportingMetricReader,
+  ConsoleMetricExporter,
+} = require('@opentelemetry/sdk-metrics');
+
+const sdk = new NodeSDK({
+  traceExporter: new ConsoleSpanExporter(),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new ConsoleMetricExporter(),
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start();
+
+const { Resource } = require("@opentelemetry/resources");
+const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+//const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
+const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
+const { trace } = require("@opentelemetry/api");
+//Instrumentations
+const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
+const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+//Exporter
+module.exports = (serviceName) => {
+    const exporter = new JaegerExporter({
+        serviceName: serviceName,
+        endpoint: "http://localhost:14268/api/traces", // adjust to your Jaeger endpoint
+    });
+ 
+   const provider = new NodeTracerProvider({
+       resource: new Resource({
+           [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+       }),
+   });
+   provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+   provider.register();
+   registerInstrumentations({
+       instrumentations: [
+           new HttpInstrumentation(),
+           new ExpressInstrumentation(),
+           new MongoDBInstrumentation(),
+       ],
+       tracerProvider: provider,
+   });
+   return trace.getTracer(serviceName);
+};
